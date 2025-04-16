f435921aebf2ac9089a6991b6b17becef39f7f3d
harshitsinghal11 <harshitsinghal.dev@outlook.com>
2025-04-16 18:05:58 +0530

    improveUI

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -145,151 +145,39 @@ const about = {
     institutions: [
       {
         name: "Class 10th",
-        description: <>Studied From Aggarwal Public School, Ballabgarh</>,
+        description: <>Studied From Aggarwal Public School, Ballabgarh Secured 79.2%</>,
       },
       {
         name: "Class 12th",
-        description: <>Studied From Aggarwal Public School, Ballabgarh</>,
+        description: <>Studied From Aggarwal Public School, Ballabgarh Secured 86.4%</>,
       },
       {
         name: "Manav Rachna Univerisity",
-        description: <>Studing BTech in CSE</>,
+        description: <>Studing BTech in Computer Science engineering</>,
       },
     ],
   },
   technical: {
-    display: true, // set to false to hide this section
+    display: true,
     title: "Technical skills",
     skills: [
       {
         title: "Figma",
         description: <>Proefficeint in Prototyping Any Website, Application Designs</>,
         // optional: leave the array empty if you don't want to display images
-        images: [
-          {
-            src: "",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-        ],
+        images: [],
       },
       {
-        title: "Next.js",
-        description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
-        images: [
-          {
-            src: "/images/projects/project-01/cover-04.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-        ],
+        title: "Photoshop",
+        description: <>Proefficeint in Basic Photoediting</>,
+        images: [],
+      },
+      {
+        title: "Web Development",
+        description: <>Able to Build Websites from Scratch Using HTML/CSS/JS</>,
+        images: [],
       },
     ],
   },
 };
-
-const blog = {
-  display: false,
-  label: "Blog",
-  title: "Writing about design and tech...",
-  description: `Read what ${person.name} has been up to recently`,
-  // Create new blog posts by adding a new .mdx file to app/blog/posts
-  // All posts will be listed on the /blog route
-};
-
-const work = {
-  label: "Work",
-  title: "My projects",
-  description: `Design and dev projects by ${person.name}`,
-  // Create new project pages by adding a new .mdx file to app/blog/posts
-  // All projects will be listed on the /home and /work routes
-};
-
-const gallery = {
-  label: "Gallery",
-  title: "My photo gallery",
-  description: `A photo collection by ${person.name}`,
-  // Images from https://pexels.com
-  images: [
-    {
-      src: "/images/gallery/img-01.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/img-02.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-03.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/img-04.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-05.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-06.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/img-07.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-08.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/img-09.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-10.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-11.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
-      src: "/images/gallery/img-12.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-13.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/img-14.jpg",
-      alt: "image",
-      orientation: "horizontal",
-    },
-  ],
-};
-
 export { person, social, newsletter, home, about, blog, work, gallery };
