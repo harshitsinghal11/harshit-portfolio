a9fb0961978d41d7105776cd54aae3b406f0c994
harshitsinghal11 <harshitsinghal.dev@outlook.com>
2025-04-16 18:20:41 +0530

    improveUI

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -3,9 +3,6 @@ const baseURL = "demo.magic-portfolio.com";
 const routes = {
   "/": true,
   "/about": true,
-  // "/work": true,
-  // "/blog": true,
-  // "/gallery": true,
 };
 
 // Enable password protection on selected routes
